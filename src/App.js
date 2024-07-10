197fd6e479027e5a8008bd2a6093bdaed40afcd8
ZanibShahid <zanib.shahid.55@gmail.com>
2024-07-10 22:25:44 +0500

    project of adding cards from array

--- src/App.js
+++ src/App.js
@@ -1,76 +1,29 @@
 import './App.css';
-import jokeData from './components/jokeData';
-// import Joke from './components/Joke';
-// import jokeData from './components/jokeData';
-
+import data from './components/data.js';
+import NavBar from './components/NavBar.js';
+import Card from './components/Card.js';
 
 function App() {
-   const card = jokeData.map(()=>{
-    return(
-      <div className="card col-4 border-0 m-3">
-                <img src="./images/card1.png" className="card-img-top" alt="..."/>
-                <div className="card-body">
-                    <div className="d-flex">
-                        <p className="ms-2">5.0 <span className="text-secondary">(6) . USA</span></p>
-                    </div>
-                    <p className="card-text lh-1">Life lessons with Katie Zaferes</p>
-                    <p className="card-text lh-1"><strong>From $136</strong> / person</p>
-                </div>
-      </div>
+  const cards = data.map(item => {
+    return (
+        <Card 
+            img={item.coverImg}
+            rating={item.stats.rating}
+            reviewCount={item.stats.reviewCount}
+            location={item.location}
+            title={item.title}
+            price={item.price}
+        />
     )
-   })
-  // const jokeElements = jokeData.map(joke=>{
-  //   return <Joke setup={joke.setup} punchline={joke.punchline}/>
-  // })
-
+  }) 
   return (
-  //  <div>
-  //   <Joke 
-  //   setup="I got my daughter a fridge for her birthday." 
-  //   punchline="I can't wait to see her face light up when she opens it."
-  //   upvotes={2}
-  //   downvotes={4}
-  //   isPun={true}
-    
-  //   />
-  //   <Joke 
-  //   setup="How did the hacker escape the police?" 
-  //   punchline="He just ransomware!"
-  //   upvotes={6}
-  //   downvotes={0}
-  //   isPun={true}
-  //   />
-  //   <Joke 
-  //   setup="Why don't pirates travel on mountain roads?" 
-  //   punchline="Scurvy."
-  //   upvotes={9}
-  //   downvotes={3}
-  //   isPun={false}
-  //   />
-  //   <Joke 
-  //   setup="Why do bees stay in the hive in the winter?" 
-  //   punchline="Swarm."
-  //   upvotes={1}
-  //   downvotes={4}
-  //   isPun={false}
-  //   />
-  //   <Joke 
-  //   setup="What's the best thing about Switzerland?" 
-  //   punchline="I don't know, but the flag is a big plus!"
-  //   upvotes={7}
-  //   downvotes={1}
-  //   isPun={true}/>
-  //   <Joke 
-  //   punchline="It’s hard to explain puns to kleptomaniacs because they always take things literally."
-  //   upvotes={0}
-  //   downvotes={8}
-  //   isPun={false}
-  //   comments={[{author: "", body: "", title: ""},]}
-  //   />
-  //  </div>
+ 
   <div>
-    {/* {jokeElements} */}
-    {card}
+    <NavBar/>
+    {/* <Card/> */}
+    <section className='card-list'>
+    {cards}
+    </section>
   </div>
    
   );
---
+++ src/components/NavBar.js
@@ -0,0 +1,9 @@
+
+export default function NavBar() {
+    return(
+        <nav className="shadow-sm rounded">
+            <img className="p-4" src="./images/navlogo.png"  alt="logo"/>
+            
+        </nav>
+    )
+}
