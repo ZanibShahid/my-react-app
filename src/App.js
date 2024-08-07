427cf3b022f9c9eb531c0a16186642452e3c2314
ZanibShahid <zanib.shahid.55@gmail.com>
2024-08-07 21:06:17 +0500

    ternary

--- src/App.js
+++ src/App.js
@@ -21,9 +21,9 @@ function App() {
     // <Meme/>
     <div className="state">
         <h1 className="state--title">Do I feel like going out tonight?</h1>
-        <div className="state--value" onClick={changeMind}>
-            <h1>{isGoingOut ? "Yes" : "No"}</h1>
-        </div>
+        <button className="state--value" onClick={changeMind}>
+            {isGoingOut ? "Yes" : "No"}
+        </button>
     </div>
    
   );
