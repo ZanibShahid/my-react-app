fb9b364a8d30894be0dc2f38e666f9cceb252b6f
ZanibShahid <zanib.shahid.55@gmail.com>
2024-08-07 20:59:40 +0500

    ternary

--- src/App.js
+++ src/App.js
@@ -4,9 +4,27 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  
+  // const isGoingOut = false
+    
+  // let answer = isGoingOut === true ? "Yes" : "No"  // Use ternary here
+  // if(isGoingOut === true) {
+  //     answer = "Yes"
+  // } else {
+  //     answer = "No"
+  // }
+
+  const [isGoingOut, setIsGoingOut] = React.useState(true)
+  function changeMind (){
+    setIsGoingOut(prevState => !prevState)
+  }
   return (
-    <Meme/>
+    // <Meme/>
+    <div className="state">
+        <h1 className="state--title">Do I feel like going out tonight?</h1>
+        <div className="state--value" onClick={changeMind}>
+            <h1>{isGoingOut ? "Yes" : "No"}</h1>
+        </div>
+    </div>
    
   );
 }
