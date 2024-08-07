096261f3c9bab0c06440a2ce4469d445e0dc4965
ZanibShahid <zanib.shahid.55@gmail.com>
2024-08-07 20:18:09 +0500

    assignimage

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,18 +1,18 @@
+import React from "react"
 import memesData from "./memesData"
 export default function Meme(props){
-    let url
+    // let url
+    const [memeImage, setMemeImage] = React.useState("")
+
     function getMemeImage(){
         
         const memeArray = memesData.data.memes
         const randomNumber = Math.floor(Math.random() * memeArray.length)
-        url = memeArray[randomNumber].url
-
-        return(
-            console.log("hi",randomNumber,url),
-            
-            <img src="https://picsum.photos/640/360" alt="" srcset="" />
-        )
+        // url = memeArray[randomNumber].url
+        setMemeImage(memeArray[randomNumber].url)
+        
     }
+    
     return(
         <div className="container my-5">
             <div className="form-row d-flex row g-5 mb-3">
@@ -26,6 +26,7 @@ export default function Meme(props){
                 </div>
             </div>
             <button type="submit" onClick={getMemeImage} className="btn text-light col-12" style={{backgroundImage:"linear-gradient(90.41deg, #672280 1.14%, #A626D3 100%)"}}>Get a new meme image <span><img style={{height:"15px"}} src="../images/paint.png" alt="" srcset="" /></span></button>
+            <img src={memeImage} alt="" srcset="" className="my-5" style={{maxWidth:"100%"}} />
         </div>
     )
 }
