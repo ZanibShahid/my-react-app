c91809e5fac14c8341a03395db0b8c1e74146d39
ZanibShahid <zanib.shahid.55@gmail.com>
2024-08-20 21:13:44 +0500

    passing state all around

--- src/App.js
+++ src/App.js
@@ -1,46 +1,21 @@
 import NavBar from './components/NavBar.js';
 import Meme from './components/Meme.js';
-import Star from './components/Star.js';
+import Header from './components/Header.js';
+import Body from './components/Body.js';
 import React from 'react';
 import './App.css';
 
 function App() {
-  const [contact, setContact] = React.useState({
-    firstName: "John",
-    lastName: "Doe",
-    phone: "+1 (719) 555-1212",
-    email: "itsmyrealname@example.com",
-    isFavorite: false
-  })
+  const [user, setUser] = React.useState("Joe")
 
-  // let starIcon = contact.isFavorite ? "star-filled.png" : "star-empty.png"
-    
-    function toggleFavorite() {
-        setContact(prevContact => ({
-            ...prevContact,
-            isFavorite: !prevContact.isFavorite
-        }))
-    }
 return (
   // <Meme/>
   
-        <main>
-            <article className="card">
-                <img src="./images/profile.png" className="card--image" />
-                <div className="card--info">
-                    <Star
-                      isFilled = {contact.isFavorite}
-                      handleClick = {toggleFavorite}
-                    />
-                    <h2 className="card--name">
-                        {contact.firstName} {contact.lastName}
-                    </h2>
-                    <p className="card--contact">{contact.phone}</p>
-                    <p className="card--contact">{contact.email}</p>
-                </div>
-                
-            </article>
-        </main>
+    <main>
+        <Header name = {user} />
+        <Body name = {user} />
+    </main>
+
   );
 }
 
