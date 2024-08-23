6d6e11cbf2b20ab6805b4251fcb0b3d851ea8986
ZanibShahid <zanib.shahid.55@gmail.com>
2024-08-23 21:27:03 +0500

    complete box challenge

--- src/App.js
+++ src/App.js
@@ -1,20 +1,54 @@
-import NavBar from './components/NavBar.js';
-import Meme from './components/Meme.js';
-import Header from './components/Header.js';
-import Body from './components/Body.js';
+
 import React from 'react';
+import boxes from './boxes';
 import './App.css';
+import Box from './Box';
 
-function App() {
-  const [user, setUser] = React.useState("Joe")
-
+function App(props) {
+ const [squares, setSquares] = React.useState(boxes)
+//  const styles = {
+//   backgroundColor : props.darkMode ?  "#222222" : "#cccccc"
+//  } 
+// function toggle(id) {
+//   setSquares(prevSquares => { 
+//     return prevSquares.map((square) => {
+//       return square.id === id ? {...square, on: !square.on} : square
+//     })
+//       // const newSquares = []
+//       // for(let i = 0; i < prevSquares.length; i++) {
+//       //     const currentSquare = prevSquares[i]
+//       //     if(currentSquare.id === id) {
+//       //         const updatedSquare = {
+//       //             ...currentSquare,
+//       //             on: !currentSquare.on
+//       //         }
+//       //         newSquares.push(updatedSquare)
+//       //     } else {
+//       //         newSquares.push(currentSquare)
+//       //     }
+//       // }
+//       // return newSquares
+//   })
+// }
+function toggle(id){
+  setSquares(prevSquares => {
+    return prevSquares.map((square) => {
+      return square.id === id ? {...square, on : !square.on} :square
+    })
+  })
+}
+ const squareElements = squares.map(square => (
+  <Box 
+    on={square.on} 
+    key={square.id} 
+    id={square.id} 
+    toggle={() => toggle(square.id)}/>
+  ))
 return (
-  // <Meme/>
   
-    <main>
-        <Header name = {user} />
-        <Body name = {user} />
-    </main>
+  <main>
+    {squareElements}
+  </main>
 
   );
 }
