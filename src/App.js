af3a535e30ad574d294238ba58cbd3c1512681d0
ZanibShahid <zanib.shahid.55@gmail.com>
2024-08-25 16:57:24 +0500

    && condition

--- src/App.js
+++ src/App.js
@@ -1,56 +1,23 @@
 
 import React from 'react';
-import boxes from './boxes';
 import './App.css';
-import Box from './Box';
-
+import Joke from './Joke';
+import jokesData from './jokesData';
 function App(props) {
- const [squares, setSquares] = React.useState(boxes)
-//  const styles = {
-//   backgroundColor : props.darkMode ?  "#222222" : "#cccccc"
-//  } 
-// function toggle(id) {
-//   setSquares(prevSquares => { 
-//     return prevSquares.map((square) => {
-//       return square.id === id ? {...square, on: !square.on} : square
-//     })
-//       // const newSquares = []
-//       // for(let i = 0; i < prevSquares.length; i++) {
-//       //     const currentSquare = prevSquares[i]
-//       //     if(currentSquare.id === id) {
-//       //         const updatedSquare = {
-//       //             ...currentSquare,
-//       //             on: !currentSquare.on
-//       //         }
-//       //         newSquares.push(updatedSquare)
-//       //     } else {
-//       //         newSquares.push(currentSquare)
-//       //     }
-//       // }
-//       // return newSquares
-//   })
-// }
-function toggle(id){
-  setSquares(prevSquares => {
-    return prevSquares.map((square) => {
-      return square.id === id ? {...square, on : !square.on} :square
-    })
-  })
-}
- const squareElements = squares.map(square => (
-  <Box 
-    on={square.on} 
-    key={square.id} 
-    id={square.id} 
-    toggle={() => toggle(square.id)}/>
-  ))
+  const jokeElements = jokesData.map(joke => {
+    return (
+        <Joke 
+            key={joke.id}
+            setup={joke.setup} 
+            punchline={joke.punchline} 
+        />
+    )
+})
 return (
-  
-  <main>
-    {squareElements}
-  </main>
-
-  );
+    <div>
+        {jokeElements}
+    </div>
+)
 }
 
 export default App;
---
+++ src/Joke.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+export default function Joke(props){
+    const [isShown, setIsShown] = React.useState(false)
+    function toggle() {
+        setIsShown(prevShown => !prevShown)
+    }
+    return (
+        <div>
+            {props.setup && <h3>{props.setup}</h3>}
+            {isShown && <p>{props.punchline}</p>}
+            <button onClick={toggle}>Show Punchline</button>
+            <hr />
+        </div>
+    )
+}
